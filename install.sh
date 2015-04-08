29ba43918fea04a769798222145e80d2640bdc9a
studioemrys <studio.emrys@gmail.com>
2015-04-08 13:36:15 +0200

    Version 1.0

--- install.sh
+++ install.sh
@@ -1,50 +1,61 @@
-#	!bin/bash
-#	Installatie van ip-check
-	vncClient="Merlijn Schoots"
-	vncClientEmail="merlijnschoots@gmail.com"
-	echo 
-	echo 	
-	echo "Check-IP is een eenvoudig programma waarmee uw computer ieder uur en bij iedere opstart het externe IP adres controleert. Indien dit IP adres verandert, wordt dit doorgegeven aan uw hulp op afstand via een email. Uw hulp op afstand heeft ten alle tijden het juiste IP adres nodig om op uw computer mee te kunnen kijken naar uw handelingen of eventuele probleem. In uw specifieke geval wordt er een email gestuurd naar $vncClient op het email adres $vncClientEmail" 
-	echo 
-	echo "LET OP: indien u deze persoon en dit email adres NIET herkend, ga dan NIET verder met installatie van dit programma!"
-	echo 
-	while true; do
-	    read -p "Wilt u dit programma installeren?" answer
-	    case $answer in
-	        [Jj]* ) 
-		echo ;
-		echo "Check-IP wordt geinstalleerd..."; 
-		echo ;		
-		echo ;				
-		tar xvfz checkip.tar.gz -C $HOME
-		sudo chmod +x $HOME/checkip/checkip.sh
-		echo ;
-		echo "Check-IP is geinstalleerd. Postfix zal worden geinstalleerd zodat het IP adres per email verzonden kan worden aan $vncClient. Postfix opent een kort en eenvoudig configuratieprogramma. Beantwoord de eerste vraag met OK. En kies bij de volgende vraag voor internetsite. Bij de volgende vraag wordt de emailnaam van het systeem gevraagd, klik hierbij eenvoudig weg weer op OK."	
-		echo ;		
-		sudo apt-get install postfix		
-		echo ;
-		echo ;
-		echo "Check-IP en Postfix zijn succesvol geinstalleerd!"
-		echo "Check-IP timer wordt ingesteld..."
-		echo ;		
-		crontab -l > temp
-		#echo new cron into cron file
-		echo "@reboot $HOME/checkip/checkip.sh" >> temp
-		echo "0 */4 * * * $HOME/checkip/checkip.sh" >> temp		
-		#install new cron file
-		crontab temp
-		rm temp
-		echo ;
-		echo "Check-IP timer succesvol ingesteld!"
-		echo "Check-IP installatie succesvol afgerond!"
-		echo ;
-		break;;
-	        [Nn]* ) 
-		echo ;
-		echo "Installatie afgebroken op initiatief van "$USER;
-		echo ; 
-		exit;;
-	        * ) 
-		echo "Beantwoordt de vraag met ja of nee";;
-	    esac
-	done
+#!bin/bash
+
+# dynamic-ip-notifier repository installation
+
+# Install information
+echo
+echo 
+echo "dynamic-ip-notifier repository being read...done"
+echo "dynamic-ip-notifier repository dependencies being read...done"
+echo "dynamic-ip-notifier requires following packages to be installed:"
+echo "	- postfix"
+echo
+echo "during installation your system will probably request for sudo password"
+echo
+echo "Security vulnerabillities being read...done"
+echo
+echo "dynamic-ip-notifier will email your external ip adress to a chosen email adress. This will make your system more vulnerable to security enforcements. As long as the email adress belongs to a reliable recipient, we consider using dynamic-ip-notifier as a safe procedure"
+echo
+while true; do
+    read -p "Would you like to install dynamic-ip-notifier?" answer
+    case $answer in
+        [Yy]* ) 
+	echo ;
+	echo "Installing dynamic-ip-notifier..."; 
+	echo ;		
+	echo "Unpacking packages...";	
+	tar xvfz dynamic-ip-notifier.tar.gz -C $HOME
+	echo "Installing packages...";		
+	sudo chmod +x $HOME/dynamic-ip-notifier/dynamic-ip-notifier.sh
+	echo "Installing postfix...";
+	echo "Postfix will be installed on your system. During installation a configuration menu will popup. At the first screen hit 'OK'. At the second screen hit 'internetsite'. At the third screen do not alter anything and hit 'OK'."	
+	echo ;		
+	sudo apt-get install postfix		
+	echo ;
+	echo ; 
+	echo "Adding cronjobs..."
+	echo ;		
+	crontab -l > temp
+	#echo new cron into cron file
+	echo "@reboot $HOME/dynamic-ip-notifier/dynamic-ip-notifier.sh" >> temp
+	echo "0 */4 * * * $HOME/dynamic-ip-notifier/dynamic-ip-notifier.sh" >> temp		
+	#install new cron file
+	crontab temp
+	rm temp
+	echo ;
+	echo "Configuring dynamic-ip-notifier";	
+	read -p "To which email adress would you like to send a notification email when the external IP adress changes?" vncClientEmail
+	echo $vncClientEmail > $HOME/dynamic-ip-notifier/config/config.ini	
+	echo ;
+	echo "dynamic-ip-notifier succesfully installed!" ;
+	echo ;
+	break;;
+        [Nn]* ) 
+	echo ;
+	echo "Installation cancelled by $USER";
+	echo ; 
+	exit;;
+        * ) 
+	echo "Please answer (y)es or (n)o";;
+    esac
+done
