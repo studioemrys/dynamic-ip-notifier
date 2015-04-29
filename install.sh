86d73673ae706d418bf9199b6e2d994c3840a5d5
studioemrys <studio.emrys@gmail.com>
2015-04-29 10:56:05 +0200

    Update install.sh
    - removed postfix installation
    - removed email prompt question
    - add server prompt question

--- install.sh
+++ install.sh
@@ -7,14 +7,12 @@ echo
 echo 
 echo "dynamic-ip-notifier repository being read...done"
 echo "dynamic-ip-notifier repository dependencies being read...done"
-echo "dynamic-ip-notifier requires following packages to be installed:"
-echo "	- postfix"
 echo
 echo "during installation your system will probably request for sudo password"
 echo
 echo "Security vulnerabillities being read...done"
 echo
-echo "dynamic-ip-notifier will email your external ip adress to a chosen email adress. This will make your system more vulnerable to security enforcements. As long as the email adress belongs to a reliable recipient, we consider using dynamic-ip-notifier as a safe procedure"
+echo "dynamic-ip-notifier will send your external ip adress to a chosen server adress. This will make your system more vulnerable to security enforcements. As long as the email adress belongs to a reliable recipient, we consider using dynamic-ip-notifier as a safe procedure"
 echo
 while true; do
     read -p "Would you like to install dynamic-ip-notifier?" answer
@@ -27,10 +25,6 @@ while true; do
 	tar xvfz ip-notifier.tar.gz -C $HOME
 	echo "Installing packages...";		
 	sudo chmod +x $HOME/ip-notifier/dynamic-ip-notifier.sh
-	echo "Installing postfix...";
-	echo "Postfix will be installed on your system. During installation a configuration menu will popup. At the first screen hit 'OK'. At the second screen hit 'internetsite'. At the third screen do not alter anything and hit 'OK'."	
-	echo ;		
-	sudo apt-get install postfix		
 	echo ;
 	echo ; 
 	echo "Adding cronjobs..."
@@ -44,8 +38,8 @@ while true; do
 	rm temp
 	echo ;
 	echo "Configuring dynamic-ip-notifier";	
-	read -p "To which email adress would you like to send a notification email when the external IP adress changes?" vncClientEmail
-	echo $vncClientEmail > $HOME/ip-notifier/config/config.ini	
+	read -p "To which server adress would you like to send a notification file when the external IP adress changes?" clientServerAdress
+	echo $clientServerAdress > $HOME/ip-notifier/config/config.ini	
 	echo ;
 	echo "dynamic-ip-notifier succesfully installed in $HOME/ip-notifier. To manually change the recipient email, change the email adress saved in $HOME/ip-notifier/config/config.ini" ;
 	echo ;
